68e2962ea8526df7311f527963c72fdaa5bbb099
Viktor Vasylkovskyi <viktor@rely.io>
2025-04-22 16:57:20 +0100

    Fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,29 +4,32 @@ export const Footer = () => {
   return (
     <footer className='py-6 md:py-0'>
       <div className='container-wrapper'>
-        <div className='container py-4'>
+        <div className='container'>
           <div className='footer-container text-balance text-center text-sm leading-loose md:text-left'>
-            Built by
-            <Link
-              href='https://github.com/vvasylkovskyi'
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-              style={{ marginLeft: 5 }}
-            >
-              Viktor Vasylkovskyi
-            </Link>
-            . The source code is available on
-            <Link
-              href='https://github.com/vvasylkovskyi/vvasylkovskyi-portfolio'
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-              style={{ marginLeft: 5 }}
-            >
-              GitHub
-            </Link>
-            .
+            <div>
+              Built by
+              <Link
+                href='https://github.com/vvasylkovskyi'
+                target='_blank'
+                rel='noreferrer'
+                className='font-medium underline underline-offset-4'
+                style={{ marginLeft: 5 }}
+              >
+                Viktor Vasylkovskyi
+              </Link>
+            </div>
+            <div>
+              . The source code is available on
+              <Link
+                href='https://github.com/vvasylkovskyi/vvasylkovskyi-portfolio'
+                target='_blank'
+                rel='noreferrer'
+                className='font-medium underline underline-offset-4'
+                style={{ marginLeft: 5 }}
+              >
+                GitHub
+              </Link>
+            </div>
           </div>
         </div>
       </div>
