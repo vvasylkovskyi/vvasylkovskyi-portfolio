c2075a0b226cf1bf9168962b614694a655cd358e
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-01 13:57:27 +0100

    Add logs

--- src/app/api/get-all-posts/get-all-posts.ts
+++ src/app/api/get-all-posts/get-all-posts.ts
@@ -3,12 +3,18 @@ import { pool } from '../database/pool';
 
 export const getBlogsData = async (): Promise<PostType[]> => {
   // NextJS hack - https://github.com/vercel/next.js/discussions/46544#discussioncomment-11136615
+
+  console.log(">>> Running in NEXT_PHASE: ", process.env.NEXT_PHASE);
+
   if (process.env.NEXT_PHASE === 'phase-production-build') return [];
 
+  console.log(">>> Currently the pool is operational: ", pool);
   try {
     const result = await pool.query(
       'SELECT slug AS url, title, meta_text AS "metaText", date, categories FROM blogs'
     );
+
+    console.log(">>> the results are: ", result.rows);
     return result.rows as PostType[];
   } catch (e) {
     throw new Error(`Error While Fetching Posts: ${e}`);
