48d36844ca7f5d3bb4d7998a983ed212f477190e
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-01 11:24:38 +0100

    Fix build

--- src/app/api/get-post-by-id/get-post-by-id.ts
+++ src/app/api/get-post-by-id/get-post-by-id.ts
@@ -2,6 +2,9 @@ import type { PostType } from '@/types/post';
 import { pool } from '../database/pool';
 
 export const getBlogById = async (url: string): Promise<PostType> => {
+  // NextJS hack - https://github.com/vercel/next.js/discussions/46544#discussioncomment-11136615
+  if (process.env.NEXT_PHASE === 'phase-production-build') return {} as PostType;
+
   try {
     const result = await pool.query(
       `SELECT 
