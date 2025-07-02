87a8fffa8f833ae37a8818ef5249669f56e452a5
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-07-02 16:06:11 +0100

    Add IAM for deploy

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -23,118 +23,20 @@ resource "aws_iam_policy" "terraform_deployer_policy" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-      # EC2, VPC, Security Groups
       {
         Effect = "Allow",
         Action = [
-          "ec2:Describe*",
-          "ec2:Create*",
-          "ec2:Delete*",
-          "ec2:Modify*",
-          "ec2:Associate*",
-          "ec2:Disassociate*",
-          "ec2:Attach*",
-          "ec2:Detach*",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:RevokeSecurityGroupIngress",
-          "ec2:AuthorizeSecurityGroupEgress",
-          "ec2:RevokeSecurityGroupEgress"
+          "s3:*",
+          "dynamodb:*",
+          "iam:*",
+          "route53:*",
+          "secretsmanager:*",
+          "rds:*",
+          "ec2:*",
+          "cloudwatch:*",
+          "logs:*"
         ],
         Resource = "*"
-      },
-
-      # RDS
-      {
-        Effect = "Allow",
-        Action = [
-          "rds:Describe*",
-          "rds:CreateDBInstance",
-          "rds:DeleteDBInstance",
-          "rds:ModifyDBInstance",
-          "rds:StartDBInstance",
-          "rds:StopDBInstance",
-          "rds:CreateDBSubnetGroup",
-          "rds:DeleteDBSubnetGroup",
-          "rds:ModifyDBSubnetGroup"
-        ],
-        Resource = "*"
-      },
-
-      # Load Balancer
-      {
-        Effect = "Allow",
-        Action = [
-          "elasticloadbalancing:*"
-        ],
-        Resource = "*"
-      },
-
-      # ACM
-      {
-        Effect = "Allow",
-        Action = [
-          "acm:DescribeCertificate",
-          "acm:ListCertificates",
-          "acm:RequestCertificate",
-          "acm:DeleteCertificate"
-        ],
-        Resource = "*"
-      },
-
-      # Route 53
-      {
-        Effect = "Allow",
-        Action = [
-          "route53:ChangeResourceRecordSets",
-          "route53:GetHostedZone",
-          "route53:ListHostedZones",
-          "route53:ListResourceRecordSets"
-        ],
-        Resource = "*"
-      },
-
-      # Secrets Manager
-      {
-        Effect = "Allow",
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:PutSecretValue",
-          "secretsmanager:CreateSecret",
-          "secretsmanager:UpdateSecret",
-          "secretsmanager:DeleteSecret",
-          "secretsmanager:DescribeSecret",
-          "secretsmanager:ListSecrets"
-        ],
-        Resource = "*"
-      },
-
-      # Terraform S3 Backend
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket",
-          "s3:GetBucketLocation"
-        ],
-        Resource = [
-          "arn:aws:s3:::${var.backend_bucket}",
-          "arn:aws:s3:::${var.backend_bucket}/*"
-        ]
-      },
-
-      # Terraform DynamoDB Lock Table
-      {
-        Effect = "Allow",
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DescribeTable"
-        ],
-        Resource = "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.lock_table}"
       }
     ]
   })
